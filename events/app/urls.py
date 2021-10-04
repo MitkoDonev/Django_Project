28ccac27843c08311a4a1495157e5a2586b9571f
MitkoDonev <MitkoDonev>
2021-10-04 09:52:49 +0300

    general forms

--- events/app/urls.py
+++ events/app/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
         r"(?P<year>[0-9]{4})/(?P<month>0?[1-9]|1[0-2])/", views.index, name="index"
     ),
     path("events/", views.get_events, name="show-events"),
+    path("add_venue/", views.add_venue, name="add-venue"),
 ]
